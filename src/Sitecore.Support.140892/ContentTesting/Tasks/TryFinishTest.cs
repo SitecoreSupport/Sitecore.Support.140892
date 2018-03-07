b1706180f20ca78044950530c684e0b3bad1294d
DK\DTAT <dtat@sitecore.net>
2018-03-07 13:46:16 +0200

    Initial commit. The bug is reproduced.

---
+++ src/Sitecore.Support.140892/ContentTesting/Tasks/TryFinishTest.cs
@@ -0,0 +1,75 @@
+﻿namespace Sitecore.Support.ContentTesting.Tasks
+{
+    using Sitecore.ContentTesting;
+    using Sitecore.ContentTesting.Data;
+    using Sitecore.ContentTesting.Diagnostics;
+    using Sitecore.ContentTesting.Intelligence;
+    using Sitecore.ContentTesting.Model.Data.Items;
+    using System;
+
+    public class TryFinishTest
+    {
+        private readonly IContentTestStore contentTestStore;
+
+        public bool UpdateTestStatistics
+        {
+            get;
+            set;
+        }
+
+        public TryFinishTest() : this(null)
+        {
+        }
+
+        public TryFinishTest(IContentTestStore contentTestStore)
+        {
+            this.contentTestStore = (contentTestStore ?? ContentTestingFactory.Instance.ContentTestStore);
+            this.UpdateTestStatistics = true;
+        }
+
+        public void Execute(Sitecore.Data.Items.Item[] items, Sitecore.Tasks.CommandItem command, Sitecore.Tasks.ScheduleItem schedule)
+        {
+            if (!Sitecore.ContentTesting.Configuration.Settings.IsAutomaticContentTestingEnabled)
+            {
+                return;
+            }
+            this.SetPropertiesFromItem(command);
+            IntelligenceService intelligenceService = new IntelligenceService();
+            for (int i = 0; i < items.Length; i++)
+            {
+                Sitecore.Data.Items.Item item = items[i];
+                try
+                {
+                    TestDefinitionItem testDefinitionItem = new TestDefinitionItem(item);
+                    if (string.IsNullOrEmpty(testDefinitionItem.WinnerCombination))
+                    {
+                        Sitecore.Data.Database database = item.Database;
+                        if (database.GetItem(new Sitecore.Data.DataUri(testDefinitionItem.ContentItem)) != null)
+                        {
+                            if (this.UpdateTestStatistics)
+                            {
+                                intelligenceService.CalculateStatisticalRelevancy(testDefinitionItem);
+                            }
+                            Sitecore.Data.ID deviceId = testDefinitionItem.Device.TargetID ?? Sitecore.Data.Items.DeviceItem.ResolveDevice(testDefinitionItem.Database).ID;
+                            ITestConfiguration testConfiguration = this.contentTestStore.LoadTest(testDefinitionItem, deviceId);
+                            if (testConfiguration != null)
+                            {
+                                this.contentTestStore.TestDeploymentManager.RunFinishTestBehavior(testConfiguration);
+                            }
+                        }
+                    }
+                }
+                catch (Exception owner)
+                {
+                    Logger.Error("Error while trying to finish tests: ", owner);
+                }
+            }
+        }
+
+        protected virtual void SetPropertiesFromItem(Sitecore.Tasks.CommandItem command)
+        {
+            TryFinishTestsItem tryFinishTestsItem = new TryFinishTestsItem(command.InnerItem);
+            this.UpdateTestStatistics = tryFinishTestsItem.UpdateTestStatistics;
+        }
+    }
+}
---
+++ src/Sitecore.Support.140892/Hooks/UpdateTaskItem.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.Hooks
+{
+    using Sitecore.Configuration;
+    using Sitecore.Diagnostics;
+    using Sitecore.Events.Hooks;
+    using Sitecore.SecurityModel;
+    using System;
+
+    public class UpdateTaskItem : IHook
+    {
+        public void Initialize()
+        {
+            using (new SecurityDisabler())
+            {
+                var databaseName = "master";
+                var itemPath = "/sitecore/system/Tasks/Commands/Content Testing/Try Finish Test";
+                var fieldName = "Type";
+
+                var type = typeof(Sitecore.Support.ContentTesting.Tasks.TryFinishTest);
+
+                var typeName = type.FullName;
+                var assemblyName = type.Assembly.GetName().Name;
+                var fieldValue = $"{typeName}, {assemblyName}";
+
+                var database = Factory.GetDatabase(databaseName);
+                var item = database.GetItem(itemPath);
+
+                if (string.Equals(item[fieldName], fieldValue, StringComparison.Ordinal))
+                {
+                    // already installed
+                    return;
+                }
+
+                Log.Audit($"Installing {assemblyName}", this);
+                Log.Info($"Updating {item.Paths.FullPath}", this);
+
+                item.Editing.BeginEdit();
+                item[fieldName] = fieldValue;
+                item.Editing.EndEdit();
+            }
+        }
+    }
+}
